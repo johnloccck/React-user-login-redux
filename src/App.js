565cfed1a6d35c4a4c35df77c43ccd6b39fd0fa3
filipjovicccc <filipjovicccc@gmail.com>
2023-01-06 23:26:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+    <Profile />
+    <Login />
+      <h1>Hello wordl</h1>
+    </div>
+  );
+}
+
+export default App;
